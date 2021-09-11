a99fc364312a856d8b1f09134f56c9a1c90ce957
uc-wilso6jm <wilso6jm@mail.uc.edu>
2021-09-11 16:19:47 -0400

    powershell files

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -3,5 +3,13 @@
 }
 
 $IP = getIP
-Write-Host("This machine's IP is $IP")
-Write-Host("This machine's IP is {0}" -f $IP)
+$USER = $env:USERNAME
+$HOSTNAME = $env:COMPUTERNAME
+$PV = $HOST.Version.Major
+$DATE = Get-Date -Format "dddd MM/dd/yyyy"
+$BODY = "This machine's IP is $IP, User is $USER, Hostname is $HOSTNAME, Powershell Version $PV, Today's Date is $DATE."
+
+"$BODY" | Out-file -FilePath .\SysInfo.txt 
+
+#Mail command kept timing out and wouldn't work
+#Send-MailMessage -To wilso6jm@mail.uc.edu -From jarod.wilson51@gmail.com -Subject "IT3038c Windows SysInfo" -Body $BODY -SmtpServer smtp.google.com -port 587 -UseSSL -Credential (Get-Credential)
