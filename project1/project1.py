d53d756ca819fdd406415a952aa785ed4c168f02
uc-wilso6jm <wilso6jm@mail.uc.edu>
2021-09-27 22:36:22 -0400

    final project 1 code

--- project1/project1.py
+++ project1/project1.py
@@ -1,9 +1,9 @@
 import string
 import random
 
-#chars=string.ascii_uppercase + string.ascii_lowercase + string.digits
+def pwGen():
+    pwSize=int(input('How many characters would you like your password to be? '))
+    newPass = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(pwSize))
+    print(newPass)
 
-def pwGen(pwSize=str(input('How many characters would you like your password to be? '))):
-    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(pwSize))
-
-print(pwGen)
+pwGen()
