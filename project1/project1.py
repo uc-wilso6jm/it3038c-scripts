21df48f950bffad26af11deac658894e81e37511
uc-wilso6jm <wilso6jm@mail.uc.edu>
2021-09-27 23:17:46 -0400

    added punctation to arg

--- project1/project1.py
+++ project1/project1.py
@@ -3,7 +3,7 @@ import random
 
 def pwGen():
     pwSize=int(input('How many characters would you like your password to be? '))
-    newPass = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(pwSize))
+    newPass = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation) for _ in range(pwSize))
     print(newPass)
 
 pwGen()
