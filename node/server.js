3f7c6a56a1b9faef1a87308020c829fc3748883f
uc-wilso6jm <wilso6jm@mail.uc.edu>
2021-09-30 22:28:23 -0400

    Added node files

--- node/server.js
+++ node/server.js
@@ -1,13 +1,24 @@
 const { fstat } = require("fs");
 var http = require("http");
 
-var server = http.createServer(function(req, res){
-    if (req.url==="/") {
+function getUptime() {
+    serverTime = os.uptime();
+    return serverTime
+}
+
+var server = http.createServer(function(req, res)
+{
+    if (req.url==="/") 
+    {
         res.writeHead(200, {"content-Type":"text/html"});
-        fstat.readFile("./public/index.html", "UTF-8", function(err, body){
+        fstat.readFile("./public/index.html", "UTF-8", function(err, body)
+        {
             res.writeHead(200, {"Content-Type":"text/text"});
             res.end(body);
-        })};
+        });
+    }
+}
+
 
 
 server.listen(3000)
