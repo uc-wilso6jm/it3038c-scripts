add8782c40348bb068a90201d8d6a5e59e87af64
uc-wilso6jm <wilso6jm@mail.uc.edu>
2021-10-10 18:36:58 -0400

    uptime works

--- node/server.js
+++ node/server.js
@@ -5,22 +5,20 @@ var ip = require('ip');
 
 var uptime_sec = os.uptime();
 var uptime_min = uptime_sec/60;
-var uptime_hour = parseInt(uptime_min/60);
+var uptime_hour = uptime_min/60;
 var uptime_day = uptime_hour/24;
 
 function getUpTime() {
-    serverTime = (uptime_day + " days " + uptime_hour + " hour(s) " + uptime_min + " minute(s) and " + uptime_sec + "second(s)");
     uptime_sec = Math.floor(uptime_sec);
     uptime_min = Math.floor(uptime_min);
-    uptime_hour = Math.floor(uptime_hour/60);
+    uptime_hour = Math.floor(uptime_hour);
     uptime_day = Math.floor(uptime_day);
 
-    uptime_day = uptime_day%60;
-    uptime_hour = uptime_hour%60;
+    uptime_hour = uptime_hour%24;
     uptime_min = uptime_min%60;
     uptime_sec = uptime_sec%60;
 
-    return serverTime
+    return (uptime_day + " days " + uptime_hour + " hour(s) " + uptime_min + " minute(s) and " + uptime_sec + " second(s)");
 }
 
 http.createServer(function(req, res){
