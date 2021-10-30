48a9b514ab6f447b082d94e203ed7a31a1e485cf
uc-wilso6jm <wilso6jm@mail.uc.edu>
2021-10-30 14:36:25 -0400

    updated project2.py

--- project2/project2.py
+++ project2/project2.py
@@ -9,8 +9,8 @@ def pwGen():
     genPW = ('Randomly Generated Password: ' + newPass) # Print new randomized string
     return genPW
 
-while pwSize < 8: # While loop checks if the value entered is less than 8. If it is, you are prompted that the password is too short and to re-enter a new length greater than 8.
-    print('Not enough characters for a strong password. Please enter at least 8 characters.')
+while pwSize < 8: # While loop checks if the value entered is less than 8. If it is, you are prompted that the length is too short and to re-enter a new length greater than 8.
+    print('Not enough characters for a strong password. Please enter a value for at least 8 characters.')
     pwSize=int(input('How many characters would you like your password to be? '))
 else:
     print(pwGen())
