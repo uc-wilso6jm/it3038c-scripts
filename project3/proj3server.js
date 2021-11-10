54c0270364fea9c8c1f783b52f9c67b3893b25bb
Wilson <Jarod.Wilson@empowermm.com>
2021-11-10 17:00:50 -0500

    updated proj3server

--- project3/proj3server.js
+++ project3/proj3server.js
@@ -1,13 +1,32 @@
 var http = require("http"); 
 var fs = require("fs");
 
-var server = http.createServer(function(req, res){ 
+http.createServer(function(req, res){
+
     if (req.url === "/") {
         fs.readFile("index.html", "UTF-8", function(err, body){
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(body);
+    });
+}
+    else if(req.url.match("/pwgen")) {
+        html=`    
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Project 3 - Password Generator</title>
+          </head>
+          <body>
+                      
+          </body>
+        </html>` 
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(html);
+    }
+    else {
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end(`404 File Not Found at ${req.url}`);
+    }
+}).listen(3000);
 
-}); 
- 
-server.listen(3000); 
-console.log("Server is listening on port 3000"); 
+console.log("Server listening on port 3000");
