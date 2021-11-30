2978b3a0688a10ab5e140c6e92081642ca1fa773
uc-wilso6jm <wilso6jm@mail.uc.edu>
2021-11-30 18:54:25 -0500

    working proj3

--- project3/routes.py
+++ project3/routes.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import string
 import random
-numChars = 0
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -10,12 +9,8 @@ app.config.from_object(__name__)
 def hello():
     return render_template("index.html")
 
-@app.route('/pwgen', methods=['POST'])
-def pwGen():
-    numChars = request.form['pwSize']
-    return render_template("pwgen.html", pwSize = numChars)
-
 @app.route('/genPW', methods=['POST'])
 def genPW():
+    numChars = int(request.form['pwSize'])
     passwd = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(numChars))
-    return render_template("genPW.html", numChars = passwd)
+    return render_template("genPW.html", passwd = passwd)
