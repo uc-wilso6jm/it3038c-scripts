c5ddad749e696168ce2e58f85ed86482f2bc96cc
uc-wilso6jm <wilso6jm@mail.uc.edu>
2021-12-02 15:25:57 -0500

    added comments

--- project3/routes.py
+++ project3/routes.py
@@ -5,12 +5,12 @@ import random
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-@app.route('/')
+@app.route('/') #route for index home page
 def hello():
     return render_template("index.html")
 
-@app.route('/genPW', methods=['POST'])
+@app.route('/genPW', methods=['POST']) #route for generated password page after submitting the form
 def genPW():
     numChars = int(request.form['pwSize'])
-    passwd = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(numChars))
+    passwd = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(numChars)) #randomizes a string of numbers, letters, and special characters for the amount of characters in pwSize
     return render_template("genPW.html", passwd = passwd)
